0a627d411ac144e199af28fb9d2db359f3778224
Jan Wagner <waja@cyconet.org>
2022-02-01 07:50:53 +0100

    check for the mysql socket

--- .github/prepare.sh
+++ .github/prepare.sh
@@ -132,6 +132,7 @@ case "$distro_id" in
 	sed -i "s/# socket =.*mysql.sock/socket=\/var\/run\/mysqld\/mysqld.sock/" /etc/my.cnf /etc/my.cnf.d/*.cnf
 	mkdir -p /var/lib/mysql/ /var/log/mariadb /var/log/mysql  && mysql_install_db > /dev/null && chown -R mysql /var/lib/mysql/ /var/log/mariadb /var/log/mysql && cd '/usr' ; /usr/bin/mysqld_safe --datadir='/var/lib/mysql' --nowatch
 	cd $HOME
+	grep -r socket /etc/my.cnf*
 	sleep 3
 	;;
 esac
